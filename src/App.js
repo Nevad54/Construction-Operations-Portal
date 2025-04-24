c90aa28aa32d14b0d61c5eb8d6171d2529359f4f
Nevad54 <carldavenquimoyog@gmail.com>
2025-04-25 07:34:09 +0800

    animation

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import { HashRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
+import { AnimatePresence } from 'framer-motion';
 import { ProjectProvider } from './context/ProjectContext';
 import Home from './Home';
 import About from './About';
@@ -10,22 +11,33 @@ import Safety from './Safety';
 import Projects from './components/Projects';
 import Contact from './Contact';
 import Admin from './components/Admin';
+import PageAnimation from './components/PageAnimation';
+
+function AnimatedRoutes() {
+  const location = useLocation();
+  
+  return (
+    <AnimatePresence mode="wait">
+      <Routes location={location} key={location.pathname}>
+        <Route path="/" element={<PageAnimation><Home /></PageAnimation>} />
+        <Route path="/about" element={<PageAnimation><About /></PageAnimation>} />
+        <Route path="/services" element={<PageAnimation><Services /></PageAnimation>} />
+        <Route path="/vision-mission" element={<PageAnimation><VisionMission /></PageAnimation>} />
+        <Route path="/core-values" element={<PageAnimation><CoreValues /></PageAnimation>} />
+        <Route path="/safety" element={<PageAnimation><Safety /></PageAnimation>} />
+        <Route path="/projects" element={<PageAnimation><Projects /></PageAnimation>} />
+        <Route path="/contact" element={<PageAnimation><Contact /></PageAnimation>} />
+        <Route path="/admin" element={<PageAnimation><Admin /></PageAnimation>} />
+      </Routes>
+    </AnimatePresence>
+  );
+}
 
 function App() {
   return (
     <ProjectProvider>
       <Router>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/services" element={<Services />} />
-          <Route path="/vision-mission" element={<VisionMission />} />
-          <Route path="/core-values" element={<CoreValues />} />
-          <Route path="/safety" element={<Safety />} />
-          <Route path="/projects" element={<Projects />} />
-          <Route path="/contact" element={<Contact />} />
-          <Route path="/admin" element={<Admin />} />
-        </Routes>
+        <AnimatedRoutes />
       </Router>
     </ProjectProvider>
   );
