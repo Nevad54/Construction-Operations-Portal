bd946a883feb2e015f4a53eebde5dc0b0b30d30a
Nevad54 <carldavenquimoyog@gmail.com>
2025-04-24 13:33:27 +0800

    Update Contact.js

--- src/Contact.js
+++ src/Contact.js
@@ -194,9 +194,7 @@ const Contact = () => {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
-          'Accept': 'application/json'
         },
-        mode: 'cors',
         credentials: 'include',
         body: JSON.stringify({
           ...formData,
