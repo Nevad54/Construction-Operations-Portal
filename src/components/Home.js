6bc84a0b9ad44b31282eb78f67143ecf52c676f4
Nevad54 <carldavenquimoyog@gmail.com>
2025-04-25 10:58:37 +0800

    p

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,6 +1,18 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import AOS from 'aos';
+import 'aos/dist/aos.css';
 
 const Home = () => {
+  // Initialize AOS
+  useEffect(() => {
+    AOS.init({
+      duration: 800,
+      once: true,
+      offset: 100,
+      easing: 'ease-in-out'
+    });
+  }, []);
+
   return (
     <div className="home">
       <section className="hero">
